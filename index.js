a098e306051c3b241d6c7a67b03f915ed9367ee5
m.goral <m.goral@ideo.pl>
2025-10-23 16:13:42 +0200

    Second commit add homepage with custom records fro object hobbies, prepare create form

--- index.js
+++ index.js
@@ -13,15 +13,56 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const hobbies = 'https://api.hubspot.com/crm/v3/objects/2-193739812?properties=name,time_from_first_start_in_years,how_inspired_you_to_start';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(hobbies, { headers });
+        const data = resp.data.results;
+        res.render('hobbies', { title: 'Hobbies | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'});
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', (req, res) => {
+    const create = {
+        properties: {
+            "name": req.body.name,
+            "time_from_first_start_in_years": req.body.time_from_first_start_in_years,
+            "how_inspired_you_to_start": req.body.how_inspired_you_to_start
+        }
+    }
 
+
+    //const createHobby = req.query.email;
+    // const createHobby = `https://api.hubapi.com/crm/v3/objects/2-193739812`;
+    // const headers = {
+    //     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    //     'Content-Type': 'application/json'
+    // };
+
+    // try { 
+    //     await axios.post(createHobby, create, { headers } );
+    //     res.redirect('back');
+    // } catch(err) {
+    //     console.error(err);
+    // }
+
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
